cd153d88bfd3b9e8c123caa1e625120331f98fe7
TOMAS DAVID KLAWIR <48385206@ort>
2024-05-27 08:43:26 -0300

    Nuevo proyecto mvc creado

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TP4___Klawir___Cywiner.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
