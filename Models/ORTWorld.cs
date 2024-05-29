15e604e82c55ebebf82b904e7ce937a46d959b76
TOMAS DAVID KLAWIR <48385206@ort>
2024-05-29 08:53:07 -0300

    Listas precargadas casi terminadas y primer VieBag hecho

--- Models/ORTWorld.cs
+++ Models/ORTWorld.cs
@@ -1,6 +1,6 @@
 static class ORTWorld{
-    public static List <string> ListaDestinos {get; private set;}
-    public static List <string> ListaHoteles {get; private set;}
+    public static List <string> ListaDestinos {get; private set;} = new List<string> {"Madrid", "Paris", "Maldivas", "Jamaica", "Alpes Suizos", "Eilat", "Rio de Janeiro", "Sudafrica", "Nueva York", "Estambul"};
+    public static List <string> ListaHoteles {get; private set;} = new List<string> {"eurostars-madrid-tower", "le narcisse", "maldivas", "jamaica-hotel", "alpes suizos", "eilat royal beach", "hilton copacabana", "the palace", "nueva york", "intercontinental"};
     public static List <string> ListaAereos {get; private set;}
     public static List <string> ListaExcursiones {get; private set;}
     public static Dictionary<string, Paquete> Paquetes {get; private set;}
