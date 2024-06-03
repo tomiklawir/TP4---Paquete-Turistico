8b90eee4ec0b07c7101bceb5e1c9bcfd8206e488
TOMAS DAVID KLAWIR <48385206@ort>
2024-06-03 09:39:52 -0300

    Creacion View SelectPaquete

--- obj/Debug/net8.0/TP4 - Klawir - Cywiner.AssemblyInfo.cs
+++ obj/Debug/net8.0/TP4 - Klawir - Cywiner.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TP4 - Klawir - Cywiner")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5fd6c659e2fbac1449bf739ba2f6e01b3eeba38f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3fc07ed6c434d0290bb14f5ab5caa938ea03def6")]
 [assembly: System.Reflection.AssemblyProductAttribute("TP4 - Klawir - Cywiner")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TP4 - Klawir - Cywiner")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
