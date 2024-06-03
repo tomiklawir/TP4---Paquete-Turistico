6a5ef0fcf3e0e9751ed541f5cbb25c9d38ffb646
TOMAS DAVID KLAWIR <48385206@ort>
2024-06-03 08:05:23 -0300

    Fotos clase ORTWorld subidas

--- obj/Debug/net8.0/TP4 - Klawir - Cywiner.AssemblyInfo.cs
+++ obj/Debug/net8.0/TP4 - Klawir - Cywiner.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TP4 - Klawir - Cywiner")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73d01cffa9df0206d2fb48a564b0c899e0068cdf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c80bad6b06b2ad9b2a60aa5e42b7289b7ca6fb3f")]
 [assembly: System.Reflection.AssemblyProductAttribute("TP4 - Klawir - Cywiner")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TP4 - Klawir - Cywiner")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
