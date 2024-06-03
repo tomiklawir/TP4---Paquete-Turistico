1583927626b1c3baf6bba45fd694928657b23213
TOMAS DAVID KLAWIR <48385206@ort>
2024-06-03 08:41:26 -0300

    prueba

--- obj/Debug/net8.0/TP4 - Klawir - Cywiner.AssemblyInfo.cs
+++ obj/Debug/net8.0/TP4 - Klawir - Cywiner.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TP4 - Klawir - Cywiner")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c80bad6b06b2ad9b2a60aa5e42b7289b7ca6fb3f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5fd6c659e2fbac1449bf739ba2f6e01b3eeba38f")]
 [assembly: System.Reflection.AssemblyProductAttribute("TP4 - Klawir - Cywiner")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TP4 - Klawir - Cywiner")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
